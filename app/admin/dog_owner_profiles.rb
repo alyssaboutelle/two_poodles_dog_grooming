7652fa4f12f4a67b0b7bb2787de32bf2ae33de1b
anonymous anonymous <alyssa.boutelle@kellogg.northwestern.edu>
2021-12-08 04:52:11 +0000

    Whitelist columns in DogOwnerProfile for ActiveAdmin

--- app/admin/dog_owner_profiles.rb
+++ app/admin/dog_owner_profiles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register DogOwnerProfile do
 
+ permit_params :first_name, :last_name, :phone, :home_address, :user_id, :email, :authorized_associates_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
