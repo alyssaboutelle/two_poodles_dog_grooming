c8b97fbbb257d8957d188425c0a18df6e419ff23
anonymous anonymous <alyssa.boutelle@kellogg.northwestern.edu>
2021-12-08 04:52:16 +0000

    Whitelist columns in SavedAppointment for ActiveAdmin

--- app/admin/saved_appointments.rb
+++ app/admin/saved_appointments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SavedAppointment do
 
+ permit_params :dog_id, :owner_id, :nickname_for_appointment
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
