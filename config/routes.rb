a6756ed0bd53f87ca691603315edc37aeb7a245d
anonymous anonymous <alyssa.boutelle@kellogg.northwestern.edu>
2021-12-08 04:52:07 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "dog_owner_profiles#index"
   resources :authorized_associates
   resources :saved_appointments
